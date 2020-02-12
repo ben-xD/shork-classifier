17a9afe0af94be5f232d79e4d4486c8e6bfe9c3a
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-12 20:19:17 +0000

    Improvements to computational intensity

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const brain = require('brain.js')
+const fs = require('fs')
 
-const net = new brain.NeuralNetworkGPU({ hiddenLayers: [2, 2] })
-// const net = new brain.NeuralNetwork({ hiddenLayers: [3] })
-
-const trainingData = [
-  { input: [0, 0], output: [0] },
-  { input: [0, 1], output: [1] },
-  { input: [1, 0], output: [1] },
-  { input: [1, 1], output: [0] },
+const shorkURLS = [
+  "./images/shork_1.JPG",
+  "./images/shork_2.JPG",
+  "./images/shork_3.JPG",
+  "./images/shork_4.JPG",
+  "./images/shork_5.JPG",
+  "./images/shork_6.JPG",
+  "./images/shork_7.JPG",
+  "./images/shork_8.JPG",
+  "./images/shork_9.JPG",
+  "./images/shork_10.JPG",
 ]
+
+const notShorkURLS = [
+  "./images/notshork_1.JPG",
+  "./images/notshork_2.JPG",
+  "./images/notshork_3.JPG",
+  "./images/notshork_4.JPG",
+  "./images/notshork_5.JPG",
+  "./images/notshork_6.JPG",
+  "./images/notshork_7.JPG",
+  "./images/notshork_8.JPG",
+  "./images/notshork_9.JPG",
+  "./images/notshork_10.JPG",
+]
+
+const trainingData = []
+
+// const notShorkURLS = [
+//   "./images/"
+// ]
+
+// Load images
+shorkURLS.map(url => {
+  const imageBuffer = fs.readFileSync(url)
+
+  trainingData.push({
+    input: imageBuffer,
+    output: [1]
+  })
+})
+
+notShorkURLS.map(url => {
+  const imageBuffer = fs.readFileSync(url)
+  trainingData.push({
+    input: imageBuffer,
+    output: [0]
+  })
+})
+// console.log(trainingData)
+
+const net = new brain.NeuralNetworkGPU()
+// const net = new brain.NeuralNetwork({ hiddenLayers: [3] })
 
 // NEURAL NETWORKS?: Training:
 // containing forward propagation: prediction
@@ -17,14 +62,15 @@ const trainingData = [
 // No need to back propagate? when training is completed.
 net.train(trainingData, {
   log: (err) => console.log(err),
-  logPeriod: 100
+  logPeriod: 1
 })
 
-let output = net.run([0.1, 0.1])
-console.log(output)
+// let output = net.run([0.1, 0.1])
+// console.log(output)
 
 // output = net.run([5, 5])
 // console.log(output)
 
-// output = net.run([-5, -5])
-// console.log(output)
+const image = fs.readFileSync(shorkURLS[0])
+const output = net.run(image)
+console.log(output[0])
